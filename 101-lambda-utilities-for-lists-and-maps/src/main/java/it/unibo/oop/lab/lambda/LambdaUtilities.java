9c7baf0ce61d701f51842f811f193a8fee9416fa
Lorenzo Dalmonte <lorenzo.dalmonte4@studio.unibo.it>
2022-11-28 10:17:12 +0100

    Esercizio 101, prima versione completata

--- 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
+++ 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
@@ -2,6 +2,7 @@ package it.unibo.oop.lab.lambda;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -61,7 +62,9 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Optional.filter
          */
-        return null;
+        final List<Optional<T>> l = new ArrayList<>(list.size());
+        list.forEach(t -> l.add(Optional.of(t).filter(pre)) );
+        return l;
     }
 
     /**
@@ -80,7 +83,9 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Map.merge
          */
-        return null;
+        final Map<R, Set<T>> map = new HashMap<>();
+        list.forEach(t -> map.merge(op.apply(t), new HashSet<>(Set.of(t)), (s,n) -> { s.addAll(n); return s; }));
+        return map;
     }
 
     /**
@@ -101,7 +106,9 @@ public final class LambdaUtilities {
          *
          * Keep in mind that a map can be iterated through its forEach method
          */
-        return null;
+        Map<K, V> out = new HashMap<>();
+        map.forEach((k,v) -> out.put(k, v.orElse(def.get())) );
+        return out;
     }
 
     /**
